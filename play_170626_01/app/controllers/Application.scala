3f42fe75604209bfe6d5d34e6da62f353c96b215
koreannick <koreannick@naver.com>
2017-06-26 15:07:01 +0900

    play example

---
+++ play_170626_01/app/controllers/Application.scala
@@ -0,0 +1,16 @@
+package controllers
+import play.api.mvc._
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action{
+    Ok("Hello world")
+  }
+  def hello(name: String) = Action{
+    Ok(views.html.main(name))
+  }
+
+}
